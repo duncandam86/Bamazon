2bc2190bbc5f57e61ac3f17b0426dd31021efbf4
Duncan Dam <duncandam@dhcp-10-101-120-20.wireless.northwestern.private>
2018-11-28 15:25:32 -0600

    Update both customer and manager JS

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -34,47 +34,49 @@ function productsForSale() {
             })
             userPromptForPurchase()
         });
+    connection.end()
+}
 
-    // function to prompt user to ask them which item_id and how many units they like
-    function userPromptForPurchase() {
-        console.log(chalk.yellow.bold("What would you like to buy?"));
-        // inquirer to prompt questions
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "item_id",
-                message: "Please enter the item_id that you like to purchase.",
-                filter: Number
-            },
-            {
-                type: "input",
-                name: "quantity",
-                message: "How many units do you need?",
-                filter: Number
-            }
-        ]).then(function (res) {
-            console.log(chalk.blue(`Customer has chosen item with item_id: ${chalk.red.bold(res.item_id)} and quantity: ${chalk.red.bold(res.quantity)}`))
-            // Query db to confirm that the item exists
-            connection.query(`SELECT* 
+// function to prompt user to ask them which item_id and how many units they like
+function userPromptForPurchase() {
+    console.log(chalk.yellow.bold("What would you like to buy?"));
+    // inquirer to prompt questions
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "item_id",
+            message: "Please enter the item_id that you like to purchase.",
+            filter: Number
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many units do you need?",
+            filter: Number
+        }
+    ]).then(function (res) {
+        console.log(chalk.blue(`Customer has chosen item with item_id: ${chalk.red.bold(res.item_id)} and quantity: ${chalk.red.bold(res.quantity)}`))
+        // Query db to confirm that the item exists
+        connection.query(`SELECT* 
             FROM products 
             WHERE item_id = ${res.item_id}`, function (err, results) {
-                    // console.log(results)
-                    if (err) {
-                        throw err
-                    };
-                    // make sure that item_id input was valid
-                    if (results.length === 0) {
-                        console.log(chalk.bgRed("Invalid item ID. Please enter an valid item ID"));
-                        productsForSale();
-                    }
-                    else {
-                        // if the quantity that the user asked for is less than the total inventory
-                        if (res.quantity <= results[0].stock_quantity) {
-                            console.log(chalk.black.bgGreen("We have enough inventory instock. Placing your order!"));
-                            //Update databse with new inventory after placing order
-                            connection.query(`Update products
+                // console.log(results)
+                if (err) {
+                    throw err
+                };
+                // make sure that item_id input was valid
+                if (results.length === 0) {
+                    console.log(chalk.bgRed("Invalid item ID. Please enter an valid item ID"));
+                    productsForSale();
+                }
+                else {
+                    // if the quantity that the user asked for is less than the total inventory
+                    if (res.quantity <= results[0].stock_quantity) {
+                        console.log(chalk.black.bgGreen("We have enough inventory instock. Placing your order!"));
+                        //Update databse with new inventory after placing order
+                        connection.query(`Update products
                             Set stock_quantity = ${results[0].stock_quantity - res.quantity}
-                            Where item_id = ${res.item_id}`, function (err, data) {
+                            Where item_id = ${results[0].item_id}`, function (err, data) {
                                 console.log(chalk.green(`New stock quantity of ${results[0].product_name}: ${results[0].stock_quantity - res.quantity}`));
                                 if (err) {
                                     throw err
@@ -88,19 +90,18 @@ function productsForSale() {
                                 // end connection here
                                 connection.end()
                             })
-                        }
-                        else {
-                            console.log(chalk.red(`Unfortunately, we have insufficient quantity of the product you requested
+                    }
+                    else {
+                        console.log(chalk.red(`Unfortunately, we have insufficient quantity of the product you requested
                             \n Please modify your order or order other products`));
-                            userPromptForPurchase()
-
-                        }
+                        userPromptForPurchase()
 
                     }
 
-                })
-        })
+                }
 
-    }
+            })
+    })
 
 }
+
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,4 +1,6 @@
-let mysql = require("mysql");
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const chalk = require("chalk");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -16,6 +18,110 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+    // console.log("connected as id " + connection.threadId);
+    productsForSale()
 });
 
+//function to show all products for sale
+function productsForSale() {
+    connection.query(`SELECT*
+    FROM products`, function (err, results) {
+            if (err) {
+                throw err;
+            };
+            results.forEach(function (row) {
+                console.log(`------------------------------`);
+                console.log(chalk.cyan(`item_id: ${row.item_id}
+                \nProduct: ${row.product_name}
+                \nPrice: ${row.price}`));
+                console.log(`------------------------------`);
+            })
+            // lowInventory();
+            addInventory();
+        });
+    connection.end()
+}
+
+//function to show products with low inventory
+function lowInventory() {
+    connection.query(`SELECT*
+    FROM products
+    Where stock_quantity < 120`, function (err, results) {
+            if (err) {
+                throw err;
+            };
+            console.log(`These items have low inventory`)
+            results.forEach(function (row) {
+                console.log(`------------------------------`);
+                console.log(chalk.red(`item_id: ${row.item_id}
+                \nProduct: ${row.product_name}
+                \nDepartment: ${row.department_name}
+                \nPrice: ${row.price}
+                \nQuantity: ${row.stock_quantity}`));
+                console.log(`------------------------------`);
+            })
+        });
+    connection.end()
+}
+
+//function add an inventory to stock
+function addInventory() {
+    //console.log("hello")
+    //prompt manager to add new inventory
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "item_id",
+            message: "Please enter the item ID",
+            filter: Number
+        },
+        {
+            type: "input",
+            name: "stock_quantity",
+            message: "Please enter the amount of units you would like to add",
+            filter: Number
+        }
+    ]).then(function (input) {
+        console.log(chalk.blue(`You have added ${input.stock_quantity} units of item with ID number ${input.item_id}`));
+        // Update new inventory in database
+        connection.query(`SELECT* 
+            FROM products 
+            WHERE item_id = ${input.item_id}`, function (err, results) {
+                // console.log(results)
+                if (err) {
+                    throw err
+                };
+                // make sure that item_id input was valid
+                if (results.length === 0) {
+                    console.log(chalk.bgRed("Invalid item ID. Please enter an valid item ID"));
+                    productsForSale();
+                }
+                else {
+                    console.log("Updating inventory!")
+                    // Update database with new inventory
+                    connection.query(`Update products 
+                    Set stock_quantity = ${results[0].stock_quantity + input.stock_quantity}
+                    Where item_id = ${results[0].item_id}`, function (err, data) {
+                        if (err) {
+                            throw err;
+                        };
+                        console.log(`------------------------------`);
+                        console.log(chalk.cyan(`item_id: ${results[0].item_id}
+                            \nProduct: ${results[0].product_name}
+                            \nNew Quantity: ${input.stock_quantity + results[0].stock_quantity}`));
+                        console.log(`------------------------------`);
+
+                    });
+
+                }
+            });
+    });
+}
+
+//function adding a new product
+function addNewProduct(){
+    // prompt to add new product
+    inquirer.prompt([
+        
+    ]) 
+}
